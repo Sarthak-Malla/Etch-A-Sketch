f0fe2dd0a0d843e1268cb7c980a10e5a9372c197
Sarthak-Malla <spm9513@nyu.edu>
2022-08-30 00:18:09 +0400

    Removing the Background Color

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@ const drawingPad = document.querySelector(".drawing-pad");
 for (let i = 0; i < 16*16; i++) {
     const div = document.createElement('div');
     div.classList.add("drawing-pixel");
-    div.setAttribute('style', `height: 40px; width: 40px; background-color: ${color};`)
+    div.setAttribute('style', `height: 40px; width: 40px;`)
 
     drawingPad.appendChild(div);
 }
