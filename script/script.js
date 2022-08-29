e2ebf1183d70edeef0aa771859599ce47dd6cd48
Sarthak-Malla <spm9513@nyu.edu>
2022-08-30 00:55:17 +0400

    Complete resizing grid
    Working code for the resizing of the pixels

--- script/script.js
+++ script/script.js
@@ -1,18 +1,37 @@
 const drawingPad = document.querySelector(".drawing-pad");
+const resizeBtn = document.querySelector(".grid-size-button");
+const padSize  = drawingPad.clientHeight;
 
-for (let i = 0; i < 16*16; i++) {
-    const div = document.createElement('div');
-    div.classList.add("drawing-pixel");
-    div.setAttribute('style', `height: 40px; width: 40px; margin: 0; border: 0;`)
-
-    drawingPad.appendChild(div);
+function changeSize(size) {
+    for (let i = 0; i < size*size; i++) {
+        const div = document.createElement('div');
+        div.classList.add("drawing-pixel");
+        div.setAttribute('style', `height: ${padSize/size}px; width: ${padSize/size}px; margin: 0; border: 0;`)
+    
+        drawingPad.appendChild(div);
+    }
+    
+    const pixels = document.querySelectorAll('.drawing-pixel');
+    
+    pixels.forEach((pixel) => {
+        pixel.addEventListener('mouseover', () => {
+            pixel.style.backgroundColor = "black";
+        });
+    });
 }
 
-const pixels = document.querySelectorAll('.drawing-pixel');
+resizeBtn.addEventListener('click', () => {
+    let size = prompt("Enter the Number of Squares per side [16-100]:");
+    while (size < 16 || size > 100) {
+        size = prompt("Enter the Number of Squares per side [16-100]:");
+    }
 
-pixels.forEach((pixel) => {
-    pixel.addEventListener('mouseover', () => {
-        pixel.style.backgroundColor = "black";
+    const pixels = document.querySelectorAll('.drawing-pixel');
+    pixels.forEach((pixel) => {
+        drawingPad.removeChild(pixel);
     });
+
+    changeSize(size);
 });
 
+changeSize(16);
