e08a454c66cd54a9ba01c08ebb8268a52362da75
Sarthak-Malla <spm9513@nyu.edu>
2022-08-30 00:40:45 +0400

    Draws on the 16x16 grid

--- script/script.js
+++ script/script.js
@@ -3,7 +3,16 @@ const drawingPad = document.querySelector(".drawing-pad");
 for (let i = 0; i < 16*16; i++) {
     const div = document.createElement('div');
     div.classList.add("drawing-pixel");
-    div.setAttribute('style', `height: 40px; width: 40px;`)
+    div.setAttribute('style', `height: 40px; width: 40px; margin: 0; border: 0;`)
 
     drawingPad.appendChild(div);
 }
+
+const pixels = document.querySelectorAll('.drawing-pixel');
+
+pixels.forEach((pixel) => {
+    pixel.addEventListener('mouseover', () => {
+        pixel.style.backgroundColor = "black";
+    });
+});
+
